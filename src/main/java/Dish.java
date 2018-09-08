1f907062631e32455635c49e9ceba974d3945f49
Arun Vasudevan <avasudevan@homeaway.com>
2018-09-08 08:01:21 -0500

    Playing with streams

--- src/main/java/Dish.java
+++ src/main/java/Dish.java
@@ -1,30 +1,29 @@
 public class Dish {
-
-    public Dish() {
-    }
-
-    public Dish(String name, int calories) {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
         this.name = name;
+        this.vegetarian = vegetarian;
         this.calories = calories;
+        this.type = type;
     }
-
-    private String name;
-    private int calories;
-
-    public int getCalories() {
-        return calories;
-    }
-
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public boolean isVegetarian() {
+        return vegetarian;
     }
-
+    public int getCalories() {
+        return calories;
+    }
+    public Type getType() {
+        return type;
+    }
+    @Override
+    public String toString() {
+        return name;
+    }
+    public enum Type { MEAT, FISH, OTHER }
 }
--- src/main/java/LowCalorieDish.java
+++ src/main/java/LowCalorieDish.java
@@ -1,19 +1,31 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class LowCalorieDish {
     public static void main(String[] args){
-        ArrayList<Dish> menu= new ArrayList<>();
-        menu.add(new Dish("Paneer Butter Masala", 150));
-        menu.add(new Dish("Rasamalai", 200));
+        List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 300, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH) );
 
         LowCalorieDish lowCalorieDish= new LowCalorieDish();
+        lowCalorieDish.skipStreams(menu);
         lowCalorieDish.java7LowCalorieDish(menu);
+        lowCalorieDish.java8LowCalorieDish(menu);
     }
 
-    public void java7LowCalorieDish(ArrayList<Dish> menu){
+    public void java7LowCalorieDish(List<Dish> menu){
         List<Dish> lowCaloricDishes = new ArrayList<>();
         for(Dish dish: menu) {
             if(dish.getCalories() < 400) {
@@ -29,9 +41,42 @@ public class LowCalorieDish {
         for(Dish dish: lowCaloricDishes) {
             lowCaloricDishesName.add(dish.getName());
         }
+        System.out.println("Java 7 Approach...."+lowCaloricDishes);
 
-        for (String dish: lowCaloricDishesName) {
-            System.out.println("Dish:"+dish);
-        }
+
+        List<String> names =
+            menu.stream()
+                .filter(dish -> {
+                    System.out.println("filtering:" + dish.getName());
+                    return dish.getCalories() > 300;
+                })
+                .map(dish -> {
+                    System.out.println("mapping:" + dish.getName());
+                    return dish.getName();
+                })
+                .limit(3)
+                .collect(toList());
+        System.out.println(names);
+    }
+
+    // Streams API Implementation
+    public void java8LowCalorieDish(List<Dish> menu){
+        List<String> lowCalorieDishes = menu.parallelStream()
+                                        .filter(d -> d.getCalories() < 400)
+                                        .sorted(Comparator.comparing(Dish::getCalories))
+                                        .map(Dish::getName)
+                                        .collect(toList());
+
+        System.out.println("Java 8 Approach...."+lowCalorieDishes);
+    }
+
+    public void skipStreams(List<Dish> menu){
+
+        System.out.println("Limiting Streams......");
+        menu.stream()
+            .filter(d -> d.getType().equals(Dish.Type.MEAT))
+            .map(Dish::getName)
+            .limit(2)
+            .forEach(System.out::println);
     }
 }
